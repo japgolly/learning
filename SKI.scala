e83e0e33458ddef65a58d43bf239d566f08c18f3
David Barri <japgolly@gmail.com>
2013-10-01 07:54:51 +1000

    asdf

--- SKI.scala
+++ SKI.scala
@@ -1,20 +1,21 @@
 // convert 
 
 
-def EQ[A, B >:A <:A]= true
-
 trait λ {
   type eval<:λ
   type ap[_<:λ]<:λ
 }
 
+def Eq[A<:λ, B<:λ](implicit ev: A#eval =:= B#eval) = true
+
 trait λFinn extends λ {
    type ap[n<:λ] = eval#ap[n]
 }
 
+trait ~[x<:λ,y<:λ] extends λFinn { type eval = x#ap[y]#eval }
+
 trait Ap2 extends λ { type ap2[x<:λ,y<:λ] = ap[x]#ap[y] }
 trait Ap3 extends Ap2 { type ap3[x<:λ,y<:λ,z<:λ] = ap[x]#ap[y]#ap[z] }
-
 
 trait λF1 { type ap[x<:λ]<:λ }
 trait λ1_1[F<:λF1, x<:λ] extends λFinn { type eval = F#ap[x]#eval }
@@ -49,11 +50,9 @@ trait λ3[F<:λF3] extends Ap3{
   type ap[x<:λ] = λ3_1[F,x]
 }
 
-//
 type I = λ1[λF1 { type ap[x<:λ] = x }]
 type K = λ2[λF2 { type ap[x<:λ,y<:λ] = x }]
 type S = λ3[λF3 { type ap[x<:λ,y<:λ,z<:λ] = x#ap[z]#ap[y#ap[z]] }]
-//
 /*
 trait I_ extends λF1 { type ap[x<:λ] = x }
 trait I extends λ1[I_]
@@ -70,70 +69,69 @@ type I2 = S#ap2[K,K]
 trait d1 extends λ {type eval = d1}
 trait d2 extends λ {type eval = d2}
 trait d3 extends λ {type eval = d3}
-EQ[d1, I#ap[d1]#eval]
-EQ[d1, K#ap[d1]#ap[d2]#eval]
-EQ[d1, S#ap[K]#ap[K]#ap[d1]#eval]
-EQ[d1, I2#ap[d1]#eval]
-
+Eq[d1, I#ap[d1]]
+Eq[d1, K#ap[d1]#ap[d2]]
+Eq[d1, S#ap[K]#ap[K]#ap[d1]]
+Eq[d1, I2#ap[d1]]
 
 
 type T = λ2[λF2 { type ap[x<:λ,y<:λ] = x }]
 type F = λ2[λF2 { type ap[x<:λ,y<:λ] = y }]
 
 type And = λ2[λF2 { type ap[x<:λ,y<:λ] = x#ap[y]#ap[x] }]
-EQ[T, And#ap[T]#ap[T]#eval]
-EQ[F, And#ap[T]#ap[F]#eval]
-EQ[F, And#ap[F]#ap[T]#eval]
-EQ[F, And#ap[F]#ap[F]#eval]
+Eq[T, And#ap[T]#ap[T]]
+Eq[F, And#ap[T]#ap[F]]
+Eq[F, And#ap[F]#ap[T]]
+Eq[F, And#ap[F]#ap[F]]
 
 type Or = λ2[λF2 { type ap[x<:λ,y<:λ] = x#ap[x]#ap[y] }]
-EQ[T, Or#ap[T]#ap[T]#eval]
-EQ[T, Or#ap[T]#ap[F]#eval]
-EQ[T, Or#ap[F]#ap[T]#eval]
-EQ[F, Or#ap[F]#ap[F]#eval]
-EQ[T, Or#ap2[T,T]#eval]
-EQ[T, Or#ap2[T,F]#eval]
-EQ[T, Or#ap2[F,T]#eval]
-EQ[F, Or#ap2[F,F]#eval]
+Eq[T, Or#ap[T]#ap[T]]
+Eq[T, Or#ap[T]#ap[F]]
+Eq[T, Or#ap[F]#ap[T]]
+Eq[F, Or#ap[F]#ap[F]]
+Eq[T, Or#ap2[T,T]]
+Eq[T, Or#ap2[T,F]]
+Eq[T, Or#ap2[F,T]]
+Eq[F, Or#ap2[F,F]]
 
 type If = λ3[λF3 { type ap[m<:λ,a<:λ,b<:λ] = m#ap[a]#ap[b] }]
-EQ[d1, If#ap[T]#ap[d1]#ap[d2]#eval]
-EQ[d2, If#ap[F]#ap[d1]#ap[d2]#eval]
-EQ[d1, If#ap3[T,d1,d2]#eval]
-EQ[d2, If#ap3[F,d1,d2]#eval]
-EQ[d1, If#ap[T]#ap2[d1,d2]#eval]
-EQ[d2, If#ap[F]#ap2[d1,d2]#eval]
+Eq[d1, If#ap[T]#ap[d1]#ap[d2]]
+Eq[d2, If#ap[F]#ap[d1]#ap[d2]]
+Eq[d1, If#ap3[T,d1,d2]]
+Eq[d2, If#ap3[F,d1,d2]]
+Eq[d1, If#ap[T]#ap2[d1,d2]]
+Eq[d2, If#ap[F]#ap2[d1,d2]]
 
-trait *>[x<:λ,y<:λ] extends λFinn { type eval = x#ap[y]#eval }
-EQ[d1, (If *> T *> d1 *> d2)#eval]
-EQ[d2, (If *> F *> d1 *> d2)#eval]
-
-
-def Eq[A<:λ, B<:λ](implicit ev: A#eval =:= B#eval) = true
-Eq[d1, If *> T *> d1 *> d2]
-Eq[d2, If *> F *> d1 *> d2]
+Eq[d1, If ~ T ~ d1 ~ d2]
+Eq[d2, If ~ F ~ d1 ~ d2]
 
 // Pairs
 type Ky = λ2[λF2 { type ap[x<:λ,y<:λ] = y }]
-type pair = λ3[λF3 { type ap[x<:λ,y<:λ,z<:λ] = z *> x *> y }]
-type fst = λ1[λF1 { type ap[x<:λ] = x *> K }]
-type snd = λ1[λF1 { type ap[x<:λ] = x *> Ky }]
-Eq[d1, fst *> (pair *> d1 *> d2)]
-Eq[d2, snd *> (pair *> d1 *> d2)]
+type pair = λ3[λF3 { type ap[x<:λ,y<:λ,z<:λ] = z ~ x ~ y }]
+type fst = λ1[λF1 { type ap[x<:λ] = x ~ K }]
+type snd = λ1[λF1 { type ap[x<:λ] = x ~ Ky }]
+Eq[d1, fst ~ (pair ~ d1 ~ d2)]
+Eq[d2, snd ~ (pair ~ d1 ~ d2)]
 
-type NIL = pair *> T *> T
+type NIL = pair ~ T ~ T
 type ISNIL = fst
-type CONS = λ2[λF2 { type ap[h<:λ,t<:λ] = pair *> F *> (pair *> h *> t) }]
-type HEAD = λ1[λF1 { type ap[z<:λ] = fst *> (snd *> z) }]
-type TAIL = λ1[λF1 { type ap[z<:λ] = snd *> (snd *> z) }]
+type CONS = λ2[λF2 { type ap[h<:λ,t<:λ] = pair ~ F ~ (pair ~ h ~ t) }]
+type HEAD = λ1[λF1 { type ap[z<:λ] = fst ~ (snd ~ z) }]
+type TAIL = λ1[λF1 { type ap[z<:λ] = snd ~ (snd ~ z) }]
 
-type L_d1 = CONS *> d1 *> NIL
-type L_d21 = CONS *> d2 *> L_d1
-// type L_d21 = CONS *> d2 *> (CONS *> d1 *> NIL)
+type L_d1 = CONS ~ d1 ~ NIL
+type L_d21 = CONS ~ d2 ~ L_d1
+// type L_d21 = CONS ~ d2 ~ (CONS ~ d1 ~ NIL)
+Eq[T, ISNIL ~ NIL]
+Eq[F, ISNIL ~ L_d1]
+Eq[d1, HEAD ~ L_d1]
+Eq[L_d1, TAIL ~ L_d21]
+Eq[NIL, TAIL ~ L_d1]
+Eq[T, ISNIL ~ (TAIL ~ L_d1)]
 
 
-type Yx[f<:λ] = λ1[λF1 { type ap[x<:λ] = f *> (x *> x) }] 
-type Y = λ1[λF1 { type ap[f<:λ] = Yx[f] *> Yx[f] }]
+type Yx[f<:λ] = λ1[λF1 { type ap[x<:λ] = f ~ (x ~ x) }] 
+type Y = λ1[λF1 { type ap[f<:λ] = Yx[f] ~ Yx[f] }]
 
 /*
 def contains(X, list) = list match {
@@ -142,51 +140,18 @@ def contains(X, list) = list match {
 }
 */
 
-Eq[T, ISNIL *> NIL]
-Eq[F, ISNIL *> L_d1]
-
-Eq[d1, HEAD *> L_d1]
-Eq[L_d1, TAIL *> L_d21]
-Eq[NIL, TAIL *> L_d1]
-Eq[T, ISNIL *> (TAIL *> L_d1)]
-
 type CONTAINS_[x<:λ] = λ2[λF2 {
-  type ap[f<:λ,l<:λ] = If *> (ISNIL *> l) *> F *> cmpHT[f,HEAD *> l,TAIL *> l]
-  type cmpHT[f<:λ,h<:λ,t<:λ] = Or *> cmpH[h] *> (f *> t)
+  type ap[f<:λ,l<:λ] = If ~ (ISNIL ~ l) ~ F ~ cmpHT[f,HEAD ~ l,TAIL ~ l]
+  type cmpHT[f<:λ,h<:λ,t<:λ] = Or ~ cmpH[h] ~ (f ~ t)
   type cmpH[h<:λ] = h
   }]
-type CONTAINS = λ2[λF2 { type ap[x<:λ,l<:λ] = (Y *> CONTAINS_[x]) *> l }]
-Eq[T, CONTAINS *> d1 *> L_d21]
-Eq[T, CONTAINS *> d2 *> L_d21]
-Eq[F, CONTAINS *> d3 *> L_d21]
+type CONTAINS = λ2[λF2 { type ap[x<:λ,l<:λ] = (Y ~ CONTAINS_[x]) ~ l }]
 
-
-
-type TFF = CONS *> T *> (CONS *> F *> (CONS *> F *> NIL))
-type FTF = CONS *> F *> (CONS *> T *> (CONS *> F *> NIL))
-type FFT = CONS *> F *> (CONS *> F *> (CONS *> T *> NIL))
-type FFF = CONS *> F *> (CONS *> F *> (CONS *> F *> NIL))
-Eq[F, CONTAINS *> T *> FFF]
-Eq[T, CONTAINS *> T *> FFT]
-Eq[T, CONTAINS *> T *> FTF]
-Eq[T, CONTAINS *> T *> TFF]
-
-
-
-
-
-
-
-
-
-
-trait Bool
-trait Equal <: Bool
-trait NotEqual <: Bool
-trait X[x] { type equals[y] <: Bool }
-
-trait EqTest[A] { type get[B >:A <:A] >:A <:A }
-trait EqTest2[A] extends EqTest[A] { type get[B >:A <:A] = B }
-
-implicitly[Equal =:= X[String]#equals[String]]
-implicitly[NotEqual =:= X[Int]#equals[String]]
+type TFF = CONS ~ T ~ (CONS ~ F ~ (CONS ~ F ~ NIL))
+type FTF = CONS ~ F ~ (CONS ~ T ~ (CONS ~ F ~ NIL))
+type FFT = CONS ~ F ~ (CONS ~ F ~ (CONS ~ T ~ NIL))
+type FFF = CONS ~ F ~ (CONS ~ F ~ (CONS ~ F ~ NIL))
+Eq[F, CONTAINS ~ T ~ FFF]
+Eq[T, CONTAINS ~ T ~ FFT]
+Eq[T, CONTAINS ~ T ~ FTF]
+Eq[T, CONTAINS ~ T ~ TFF]
