4cba8234c248ebf6403ea12baf66df24f5246dc0
David Barri <japgolly@gmail.com>
2014-06-05 17:57:10 +1000

    Cant get hopcroft to work as expected

--- scala-algo/src/main/scala/automata.scala
+++ scala-algo/src/main/scala/automata.scala
@@ -2,6 +2,11 @@ package golly.algo.automata
 
 import scala.annotation.tailrec
 
+//object Implicits {
+//  implicit class SetExt[A](s: Set[A]) extends AnyVal {
+//  }
+//}
+
 // ============================================================================================================
 
 case class DFA[Q, Σ](states: Set[Q], alphabet: Set[Σ], δ: (Q, Σ) => Option[Q], q0: Q, accepts: Set[Q]) {
@@ -20,6 +25,147 @@ case class DFA[Q, Σ](states: Set[Q], alphabet: Set[Σ], δ: (Q, Σ) => Option[Q
     }
 
     go(q0, language)
+  }
+
+  def smaller[A](a: Set[A], b: Set[A]) = if (a.size <= b.size) a else b
+
+  def δInv: Σ => Set[Q] => Set[Q] =
+    a => qs => states.filter(δ(_, a).fold(false)(qs contains _))
+
+  def split(b: Set[Q], s: Set[Q], a: Σ) = {
+    println(s"  δ⁻¹($a, $s) = ${δInv(a)(s)}")
+    val x = b & δInv(a)(s)
+    val y = states -- x
+    (b & x, b & y)
+  }
+
+  // http://www.dcc.fc.up.pt/dcc/Pubs/TReports/TR07/dcc-2007-03.pdf
+  def minimiseHopcroft2 = {
+    var p = Set(accepts, states -- accepts)
+    var l = Set(accepts)
+    while (l.nonEmpty) {
+      val s = l.head
+      l -= s
+      for (a <- alphabet) {
+        var p2 = p
+        for (b <- p) {
+          val (b1,b2) = split(b, s, a)
+          if (b1.nonEmpty && b2.nonEmpty) {
+            p2 = p2 - b + b1 + b2
+            l += smaller(b1, b2)
+          }
+        }
+        p = p2
+      }
+    }
+
+    minimiseWith(p)
+  }
+
+  // http://en.wikipedia.org/wiki/DFA_minimization
+  def minimiseHopcroft = {
+    var p = Set(accepts, states -- accepts)
+    var w = Set(accepts)
+    while (w.nonEmpty) {
+      val a = w.head
+      w -= a
+      println(s"A = $a, W = $w, P = $p")
+      for (c <- alphabet) {
+        val x = δInv(c)(a)
+        //println(s"Set of states for which a transition on '$c' leads to a state in $a  =  $x")
+        var p2 = p
+        for {
+          y <- p
+          intersection = x & y if intersection.nonEmpty
+          compliment = y -- x if compliment.nonEmpty
+        } {
+          println(s"  P2 = $p2 | INT = $intersection | COM = $compliment | X = $x | Y = $y")
+          p2 = p2 - y + intersection + compliment
+          println("  P2 = "+p2)
+          if (w contains y)
+            w = w - y + intersection + compliment
+          else
+            w += smaller(intersection, compliment)
+        }
+        p = p2
+      }
+    }
+
+    minimiseWith(p)
+  }
+
+  def minimiseHopcroft4 = {
+    val c0 = accepts
+    val c1 = states -- accepts
+    var p = Set(c0, c1)
+    var l = Set(smaller(c0, c1))
+    while (l.nonEmpty) {
+      val c = l.head
+      l -= c
+      println(s"C = $c, L = $l, P = $p")
+      for (a <- alphabet) {
+        println(s"---> P = $p")
+        for (b <- p) {
+          println(s"       b = $b")
+          val (b1,b2) = split(b, c, a)
+//          println(s"  a = $a, b = $b, b1|2 = $b1|$b2, P = $p")
+          if (b1.nonEmpty) {
+            p = p - b + b1 + b2
+            if (l(b))
+              l = l - b + b1 + b2
+            else
+              l += smaller(b1, b2)
+          }
+        }
+      }
+    }
+
+    minimiseWith(p)
+  }
+
+  // http://computerlabor.math.uni-kiel.de/stochastik/colloquium08/slides/carton.pdf
+  def minimiseHopcroft3 = {
+    val F = accepts
+    val Fc = states -- accepts
+    val minFFc = smaller(F, Fc)
+    var p = Set(F, Fc)
+    var w = alphabet.map(a => (minFFc, a))
+    while (w.nonEmpty) {
+      val pair = w.head
+      w -= pair
+      val (c, a) = pair
+      println(s"C = $c, a = $a, W = $w, P = $p")
+
+      var p2 = p
+      for (b <- p) {
+        val (b1, b2) = split(b, c, a)
+        if (b1.nonEmpty && b2.nonEmpty) {
+          p2 = p2 - b + b1 + b2
+          for (bb <- alphabet)
+            if (w.contains((b, bb)))
+              w = w - ((b, bb)) + ((b1, bb)) + ((b2, bb))
+            else
+              w += ((smaller(b1, b2), bb))
+        }
+      }
+      p = p2
+    }
+
+    minimiseWith(p)
+  }
+
+  def minimiseWith(equivilentStates: Set[Set[Q]]) = {
+    println(s"minimiseWith: $equivilentStates")
+    val dups = equivilentStates.filter(_.size > 1)
+    val dupToUniq = dups.foldLeft(Map.empty[Q, Q])((m, qs) => {
+      val h = qs.head
+      m ++ (qs - h).map(_ -> h).toMap
+    })
+    println(s"minimiseWith: $dupToUniq")
+    val dupQs = dupToUniq.keySet
+    def fix(q: Q): Q = dupToUniq.getOrElse(q, q)
+    val δ2: (Q, Σ) => Option[Q] = δ(_,_) map fix
+    DFA(states -- dupQs, alphabet, δ2, fix(q0), accepts -- dupQs)
   }
 }
 
--- scala-algo/src/test/scala/automata-test.scala
+++ scala-algo/src/test/scala/automata-test.scala
@@ -22,18 +22,23 @@ class AutomataTest extends Specification with ScalaCheck {
   }
 
   lazy val dfa = {
-    val Q = Set("_", "B", "O1", "O2", "T")
-    val Σ = Set('b', 'o', 't')
+    val Q = Set("_", "B", "O1", "O1U", "O2", "T")
+    val Σ = Set('b', 'o', 'O', 't')
     val δ: (String, Char) => Option[String] = {
       case ("_", 'b') => Some("B")
       case ("B", 'o') => Some("O1")
+      case ("B", 'O') => Some("O1U")
       case ("O1", 'o') => Some("O2")
       case ("O1", 't') => Some("T")
+      case ("O1U", 't') => Some("T")
       case ("O2", 't') => Some("T")
       case _ => None
     }
     DFA(Q, Σ, δ, "_", Set("T"))
   }
+
+  lazy val dfa2 = dfa.minimiseHopcroft4
+//  println(dfa2)
 
   "NFA" should {
     "match 'cat'" in { nfa.run("cat".toList) must beTrue}
@@ -43,10 +48,29 @@ class AutomataTest extends Specification with ScalaCheck {
     "not match 'caut'" in { nfa.run("caut".toList) must beFalse}
   }
 
-  "DFA" should {
+  def test(dfa: DFA[String, Char]) = {
+    println(dfa)
     "match 'bot'" in { dfa.run("bot".toList) must beTrue}
+    "match 'bot'" in { dfa.run("bot".toList) must beTrue}
+    "match 'bOt'" in { dfa.run("bOt".toList) must beTrue}
     "match 'boot'" in { dfa.run("boot".toList) must beTrue}
+    "not match 'bOot'" in { dfa.run("bOot".toList) must beFalse}
+    "not match 'boOt'" in { dfa.run("boOt".toList) must beFalse}
     "not match 'bt'" in { dfa.run("bt".toList) must beFalse}
     "not match 'boto'" in { dfa.run("boto".toList) must beFalse}
   }
+
+  "DFA" should {
+    "δInv" in {
+      var errs = List.empty[String]
+      for {q <- dfa.states; a <- dfa.alphabet; q2 <- dfa.δ(q, a)} {
+        val t = dfa.δInv(a)(Set(q2))
+        if (!t.contains(q) || t.map(dfa.δ(_, a) == q).forall(b => b))
+          errs ::= s"\n  δ($q, $a) = $q2, δInv = $t"
+      }
+      errs must beEmpty
+    }
+    test(dfa)
+  }
+  "DFA2" should {test(dfa2)}
 }
